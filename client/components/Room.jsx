4b5129247d94424b406e845c6956d8688066614b
Timothy Levi Campbell <timothylevicampbell@gmail.com>
2016-02-01 22:58:09 -0500

    Refactor RoomList and Room components

--- client/components/Room.jsx
+++ client/components/Room.jsx
@@ -2,9 +2,9 @@ Room = React.createClass({
   render() {
     return (
       <li className="room-item">
-        <a className="room-item-link" href="#"
-          title="Navigate to Room Name">
-          Room Name
+        <a className="room-item-link" href={this.props.url}
+          title={this.props.otherUser}>
+         {this.props.otherUser}
         </a>
       </li>
     );
--- client/components/RoomList.jsx
+++ client/components/RoomList.jsx
@@ -1,5 +1,26 @@
 RoomList = React.createClass({
+  getInitialState() {
+    const rooms = [
+      {
+        _id: "1",
+        url: "http://yahoo.com",
+        otherUser: "username2"
+      },
+      {
+        _id: "2",
+        url: "http://yahoo.com",
+        otherUser: "username3"
+      }
+    ];
+
+    return { rooms };
+  },
+
   render() {
+    const rooms = this.state.rooms.map((room) => {
+      return <Room key={room._id} {...room} />;
+    });
+
     return (
       <section className="room-list-section">
         <h2 className="room-list-title">Room List and Search</h2>
@@ -7,9 +28,7 @@ RoomList = React.createClass({
           type="text"
           placeholder="Search rooms" />
         <button className="room-list-clear-search">Clear search</button>
-        <ul className="room-list-items">
-          <Room />
-        </ul>
+        <ul className="room-list-items">{rooms}</ul>
       </section>
     );
   }

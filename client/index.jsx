f4c518eaf9ecd64a4ff4b246c624c11d9a927eb5
Timothy Levi Campbell <timothylevicampbell@gmail.com>
2016-02-02 10:54:11 -0500

    Add accounts ui config

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,8 @@
 if (Meteor.isClient) {
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_EMAIL"
+  });
+
   Meteor.startup(function () {
     ReactDOM.render(<App />, document.getElementById("render-target"));
   });

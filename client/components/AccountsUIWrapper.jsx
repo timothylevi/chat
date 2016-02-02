892df3742762056a956b012bd47b89c82c52b634
Timothy Levi Campbell <timothylevicampbell@gmail.com>
2016-02-02 10:52:45 -0500

    Replace user form with meteor accounts ui

---
+++ client/components/AccountsUIWrapper.jsx
@@ -0,0 +1,8 @@
+AccountsUIWrapper = React.createClass({
+  componentDidMount() {
+    this.view = Blaze.render(Template.loginButtons,
+      ReactDOM.findDOMNode(this.refs.container));
+  },
+  componentWillUnmount() { Blaze.remove(this.view); },
+  render() { return <span ref="container" />; }
+});

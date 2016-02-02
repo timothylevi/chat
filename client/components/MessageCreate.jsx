b33221832249fdbadbad2d1e0a3302ac2ef3bb2c
Timothy Levi Campbell <timothylevicampbell@gmail.com>
2016-02-02 10:37:39 -0500

    Add form to MessageCreate component

--- client/components/MessageCreate.jsx
+++ client/components/MessageCreate.jsx
@@ -2,9 +2,13 @@ MessageCreate = React.createClass({
   render() {
     return (
       <section className="message-create-section">
-        <h2 className="message-create-title">Create Message</h2>
-        <input className="message-create-input" type="text" placeholder="Type message and press 'Enter'" />
-        <button className="message-create-submit">Submit</button>
+        <form className="message-create-form" onSubmit={this.handleCreateMessage}>
+          <h2 className="message-create-title">Create Message</h2>
+          <input className="message-create-input" type="text"
+            placeholder="Type message and press 'Enter'"
+            ref="createMessageInput" />
+          <input className="message-create-submit" type="submit" value="Submit" />
+        </form>
       </section>
     );
   }
